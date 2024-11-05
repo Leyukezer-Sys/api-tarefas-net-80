11a04ee5aabbc7077d75b273cb398367792c80a6
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2024-11-05 17:07:46 -0400

    atualizacao 05-11

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,43 @@
+﻿using ApiTarefasNet80.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTarefasNet80.Controllers
+{
+    [Route("categorias")]
+    [ApiController]
+    public class CategoriaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                List<Categoria> lista = new CategoriaDAO().List();
+
+                return Ok(lista);
+            }
+            catch (Exception)
+            {
+
+                return Problem($"Ocorreram erros ao processar a Solução");
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) {
+            try
+            {
+                var categoria = new CategoriaDAO().GetById(id);
+                if (categoria == null)
+                {
+                    return NotFound();
+                }
+                return Ok(categoria);
+            }
+            catch (Exception)
+            {
+
+                return Problem("Ocorreram erros ao processar a solicitação");
+            }
+        }
+    }
+}
--- Models/CategoriaDAO.cs
+++ Models/CategoriaDAO.cs
@@ -1,4 +1,5 @@
 ﻿using ApiTarefasNet80.Database;
+using MySql.Data.MySqlClient;
 
 namespace ApiTarefasNet80.Models
 {
@@ -28,6 +29,121 @@ namespace ApiTarefasNet80.Models
             }
             catch (Exception)
             {
+                throw;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+
+        public List<Categoria> List()
+        {
+            try
+            {
+                List<Categoria> list = [];
+
+                var query = _conn.Query();
+                query.CommandText = "SELECT * FROM categorias";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new Categoria()
+                    {
+                        Id = reader.GetInt32("id_cat"),
+                        Nome = reader.GetString("nome_cat")
+                    });
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally 
+            {
+                _conn.Close();
+            }
+        }
+        public Categoria? GetById(int id)
+        {
+            try
+            {
+                Categoria _categoria = new Categoria();
+
+                var query = _conn.Query();
+                query.CommandText = "SELECT * FROM categorias WHERE id_cat = @_id";
+
+                query.Parameters.AddWithValue("@_id", id);
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    return null;
+                }
+                while (reader.Read())
+                {
+                    _categoria.Id = reader.GetInt32("id_cat");
+                    _categoria.Nome = reader.GetString("nome_cat");
+                }
+                return _categoria;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+        public void Update(Categoria item)
+        {
+            try
+            {
+                var query = _conn.Query();
+                query.CommandText = "UPDATE categorias SET nome_cat = @_nome WHERE id_cat = @_id";
+
+                query.Parameters.AddWithValue("@_nome", item.Nome);
+                query.Parameters.AddWithValue("@_id", item.Id);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0) {
+                    throw new Exception("O registro não foi atualizado. tente novamente!");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+        public void Delete(int id) 
+        {
+            try
+            {
+                var query = _conn.Query();
+                query.CommandText = "DELETE FROM categorias WHERE id_cat = @_id";
+
+                query.Parameters.AddWithValue("@_id", id);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)  throw new Exception("O Registro não foi Excluido. Tente Novamente!");
+            }
+            catch (Exception)
+            {
+
                 throw;
             }
             finally
